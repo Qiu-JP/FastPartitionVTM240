420c3f3912d88d4b1c649bfbe765016b633fbcc1
qiujunpeng <qiujunpeng.726@bytedance.com>
2026-07-01 15:49:12 +0800

    create script /create_classifier_pretrain.py: generate standard gridmap dataset for classifier pretrain

--- network/script/create_classifier_pretrain.py
+++ network/script/create_classifier_pretrain.py
@@ -1,5 +1,9 @@
 import numpy as np
 import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
 import paths
 
 CLASSIFIER_I_CLASS_NAMES = {
